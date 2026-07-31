b421a08ddb869138e709b68cd9a7f4283bd93a2d
Anurag Barmon <251-15-387@diu.edu.bd>
2026-07-31 13:42:28 +0600

    Update Complaint.java

--- src/main/java/com/opengovtbd/model/Complaint.java
+++ src/main/java/com/opengovtbd/model/Complaint.java
@@ -7,7 +7,6 @@ import java.util.List;
 import java.util.Set;
 import java.util.concurrent.atomic.AtomicLong;
 
-/** A complaint filed by a citizen against a public service issue. */
 public class Complaint {
 
     private static final AtomicLong SEQUENCE = new AtomicLong(2000);
