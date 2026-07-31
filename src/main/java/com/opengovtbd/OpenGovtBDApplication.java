11622a22867fc59bb7740c5bed7e4342296d9973
Pronob Das <251-15-155@diu.edu.bd>
2026-07-31 15:40:24 +0600

    Create OpenGovtBDApplication class for Spring Boot

---
+++ src/main/java/com/opengovtbd/OpenGovtBDApplication.java
@@ -0,0 +1,11 @@
+package com.opengovtbd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenGovtBDApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(OpenGovtBDApplication.class, args);
+    }
+}
