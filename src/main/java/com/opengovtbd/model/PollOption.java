77a524b41dc7ef5592d69ae156e939976ad9a990
Shreya <251-15-467@diu.edu.bd>
2026-08-06 01:50:19 +0600

    Update PollOption.java

--- src/main/java/com/opengovtbd/model/PollOption.java
+++ src/main/java/com/opengovtbd/model/PollOption.java
@@ -4,9 +4,19 @@ public class PollOption {
     private final String text;
     private int votes = 0;
 
-    public PollOption(String text) { this.text = text; }
+    public PollOption(String text) {
+        this.text = text;
+    }
 
-    public String getText() { return text; }
-    public int getVotes() { return votes; }
-    public void incrementVotes() { this.votes++; }
+    public String getText() {
+        return text;
+    }
+
+    public int getVotes() {
+        return votes;
+    }
+
+    public void incrementVotes() {
+        this.votes++;
+    }
 }
