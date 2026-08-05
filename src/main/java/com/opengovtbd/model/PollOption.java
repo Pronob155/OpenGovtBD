e632443b49a008525b1a6251f027def755459e90
Shreya <251-15-467@diu.edu.bd>
2026-08-06 01:49:32 +0600

    Create PollOption.java

---
+++ src/main/java/com/opengovtbd/model/PollOption.java
@@ -0,0 +1,12 @@
+package com.opengovtbd.model;
+
+public class PollOption {
+    private final String text;
+    private int votes = 0;
+
+    public PollOption(String text) { this.text = text; }
+
+    public String getText() { return text; }
+    public int getVotes() { return votes; }
+    public void incrementVotes() { this.votes++; }
+}
