5563c643ee62772021d86d372026265c2db5ae8e
Pronob Das <251-15-155@diu.edu.bd>
2026-08-07 09:59:10 +0600

    Update OpenGovtBDApplication.java

--- src/main/java/com/opengovtbd/OpenGovtBDApplication.java
+++ src/main/java/com/opengovtbd/OpenGovtBDApplication.java
@@ -3,7 +3,6 @@ package com.opengovtbd;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-
 @SpringBootApplication
 public class OpenGovtBDApplication {
     public static void main(String[] args) {
